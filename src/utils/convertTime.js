16430a2df16a5c8a98bbf81839f07f21fb93104b
elishaolade <elishaolade@gmail.com>
2021-09-27 09:01:16 -0400

    first commit

---
+++ src/utils/convertTime.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+export default Time = () => {
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+    
+}
